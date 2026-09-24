2a93bad12cc2528616a4093ec89270703e6ec193
grantkee <grant@kee.ai>
2026-09-24 13:37:51 -0500

    Hide the Essential Guide for LPs button on pool pages

--- src/components/common/PoolHeading.tsx
+++ src/components/common/PoolHeading.tsx
@@ -6,6 +6,8 @@ import LabelAddLiquidity from "./LabelAddLiquidity";
 import Button from "./Button";
 import ExtrnalLinkIcon from "../../../public/icons/ExternalLinkIconWhite.svg"
 
+const SHOW_ESSENTIAL_GUIDE_BUTTON = false; // Temporarily hidden; content at content/about/TELx-liquidity-mining-essential-guide-for-lps.md is kept.
+
 export default function PoolHeading({
   contractData,
 }: {
@@ -21,7 +23,7 @@ export default function PoolHeading({
         <PoolSnapshotAssets flex contractData={contractData} />
       </div>
       <div className="flex gap-2">
-        {contractData.protocol === "uniswap" &&
+        {SHOW_ESSENTIAL_GUIDE_BUTTON && contractData.protocol === "uniswap" &&
           <Button linkText={"Essential Guide for LPs"} external rightIcon={<ExtrnalLinkIcon height={20} width={20} />} linkUrl="/about/telx-liquidity-mining-essential-guide-for-lps" type="secondary" />
         }
         <LabelAddLiquidity contractData={contractData} />
